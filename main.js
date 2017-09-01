ae2ddb9fb256a124b9faacc7df5818e0008b36db
TY-089\Administrator <lmw6412036@sina.com>
2017-09-01 16:32:18 +0800

    Changes to be committed: 	new file:   .gitignore 	modified:   main.js 	modified:   package.json

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /**
  * Created by Administrator on 2017/6/23 0023.
  */
-const _toString = Object.prototype.toString
+var _toString = Object.prototype.toString
 module.exports = function (value) {
-    let type = _toString.call(value).replace(/[\[\]\s]|object/g, '')
+    var type = _toString.call(value).replace(/[\[\]\s]|object/g, '')
     if (type == "Number") {
         if (parseInt(value) !== value) {
             type = "Float";
